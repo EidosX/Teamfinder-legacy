8bc3a788629ec99a628c55528c9a20be90e4fe8d
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-10 09:33:30 +0200

    Added Applications API

---
+++ backend/Routes/API/ApplicationsAPI.js
@@ -0,0 +1,39 @@
+import { db, app } from '../../Globals.js'
+import { error, success } from './JsonResponses.js'
+
+app.get('/api/applications', async (req, res) => {
+  res.send(await db('Applications'))
+})
+
+app.post('/api/applications', async (req, res) => {
+  if (!res.locals.user) {
+    error(res, 'You must be connected to make an application')
+    return
+  }
+  let recruitment_id = req.body?.recruitment_id
+  let message = req.body?.message
+  if (!recruitment_id || !message) {
+    error(res, 'Arguments recruitment_id and message are mandatory')
+    return
+  }
+  if (!(await db('Recruitments').where('id', '=', recruitment_id)).length) {
+    error(res, 'Recruitment not found')
+    return
+  }
+  if (
+    (
+      await db('Applications')
+        .where('recruitment_id', '=', recruitment_id)
+        .where('user_id', '=', res.locals.user.id)
+    ).length
+  ) {
+    error(res, 'You already applied for this recruitment!')
+    return
+  }
+  await db('Applications').insert({
+    user_id: res.locals.user.id,
+    recruitment_id,
+    message
+  })
+  success(res)
+})
--- backend/Routes/Routes.js
+++ backend/Routes/Routes.js
@@ -1,3 +1,4 @@
 import './API/UsersAPI.js'
 import './API/AuthAPI.js'
 import './API/RecruitmentsAPI.js'
+import './API/ApplicationsAPI.js'
