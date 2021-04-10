6fa060f3d1f943be226b2154ca9c09b0db0d5d82
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-10 11:16:01 +0200

    Frontend top bar!

--- frontend/PUBLIC/res/js/topbar.js
+++ frontend/PUBLIC/res/js/topbar.js
@@ -1,1 +1,33 @@
-const injectTopbar = () => {}
+// This whole source file is why I like react
+// (pretend html templates don't exist btw)
+{
+  const topBarDOM = document.createElement('div')
+  topBarDOM.id = 'top-bar'
+  // Unfortunately creating an svg procedurally doesn't work, so good ol' innerHtml
+  topBarDOM.innerHTML +=
+    '<svg class="logo"><image href="/res/svg/teamfinder-logo.svg" /></svg>'
+  topBarDOM.append(makeSearchBar())
+  topBarDOM.innerHTML +=
+    '<svg class="user-icon"><image href="/res/svg/user.svg" /></svg>'
+
+  document.write('<link rel="stylesheet" href="/res/css/topbar.css">')
+  document.body.prepend(topBarDOM)
+
+  function makeSearchBar() {
+    const divDOM = document.createElement('div')
+    divDOM.className = 'search-bar'
+
+    divDOM.innerHTML +=
+      '<svg class="mag-glass icon"><image href="/res/svg/mag-glass.svg" /></svg>'
+    divDOM.innerHTML +=
+      '<svg class="arrow-right icon"><image href="/res/svg/arrow-right.svg" /></svg>'
+
+    const inputDOM = document.createElement('input')
+    inputDOM.type = 'text'
+    inputDOM.name = 'searchQuery'
+    inputDOM.placeholder = 'Rechercher'
+    divDOM.append(inputDOM)
+
+    return divDOM
+  }
+}
