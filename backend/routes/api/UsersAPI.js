4d241dcb4be989b6681f467ec2a0071ceaec4c9d
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-23 00:36:45 +0200

    Users post API

---
+++ backend/routes/api/UsersAPI.js
@@ -0,0 +1,37 @@
+import { ok, err } from './ApiTools.js'
+import bcrypt from 'bcrypt'
+import { Ranks } from '../../misc/Ranks.js'
+import db from '../../database.js'
+
+export default function usersAPI({ app }) {
+  app.post('/api/users', async (req, res) => {
+    if (res.locals.user) return res.send(err('Vous etes déjà connecté'))
+    const nickname = req.body.nickname
+    const email = req.body.email
+    const passwordPlain = req.body.password
+    if (!nickname) return res.send(err('Le pseudonyme est obligatoire'))
+    if (!email) return res.send(err("L'email est obligatoire"))
+    if (!passwordPlain) return res.send(err('Le mot de passe est obligatoire'))
+
+    if (!/^[a-zA-Z0-9_]{3,20}$/.test(nickname))
+      return res.send(err('Pseudonyme invalide'))
+    const nickAlreadyTaken = await db('Users')
+      .whereRaw('LOWER(Users.nickname) = ?', nickname.toLowerCase())
+      .first()
+    if (nickAlreadyTaken) return res.send(err('Ce pseudonyme a déjà été pris'))
+
+    let emailAlreadyTaken = await db('Users')
+      .whereRaw('LOWER(Users.email) = ?', email.toLowerCase())
+      .first()
+    if (emailAlreadyTaken) return error(res, 'Un compte existe déjà avec ce mail')
+
+    if (passwordPlain.length < 6) return res.send(err('Mot de passe trop court'))
+    if (passwordPlain.length > 20) return res.send(err('Mot de passe trop long'))
+
+    bcrypt.hash(req.body.password, 10, async (_err, password) => {
+      await db('Users').insert({ nickname, email, password, rank: Ranks.DEFAULT })
+    })
+
+    res.send(ok())
+  })
+}
--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -1,5 +1,7 @@
 import auth from './api/AuthAPI.js'
+import users from './api/UsersAPI.js'
 
-export default async function initAPIRoutes({ app }) {
+export default function initAPIRoutes({ app }) {
   auth({ app })
+  users({ app })
 }
