fdb558e9201d8d421b0a18ff19788473dfff439f
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-23 00:34:45 +0200

    Fixed wrong function call order

--- backend/main.js
+++ backend/main.js
@@ -3,7 +3,7 @@ import initPagesRoutes from './routes/pages.js'
 import initAPIRoutes from './routes/apis.js'
 import initGlobalHooks from './hooks/globalHooks.js'
 
+initGlobalHooks({ app })
 initPagesRoutes({ app })
 initAPIRoutes({ app })
-initGlobalHooks({ app })
 app.listen(process.env.PORT ?? 3000)
