2f1f1a30a82850c48a0d7e85c9238ff4d398494e
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-22 23:20:59 +0200

    Added express session

--- backend/httpApp.js
+++ backend/httpApp.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import path from 'path'
+import session from 'express-session'
 
 const app = express()
 export default app
 
+app.use(
+  session({
+    secret: 'hurle chiale rugit',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }
+  })
+)
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('views'))
 app.use('/static', express.static('res/static'))
