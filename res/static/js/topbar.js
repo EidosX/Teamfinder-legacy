a6f7054c9fc650827db7037190fdbb8a724f4938
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-23 13:10:16 +0200

    Started user menu frontend

--- res/static/js/topbar.js
+++ res/static/js/topbar.js
@@ -9,9 +9,99 @@
     </div>
     <svg class="user logo"><image href="/static/svg/user.svg" /></svg>
   </div>
-  <div id=topbar-ghost></div>
+  <div id="topbar-ghost"></div>
+  <div id="user-menu" class="hidden">
+    <div id="user-menu-ghost"></div>
+  </div>
   `
   // Inject into html
   document.write('<link rel="stylesheet" href="/static/css/topbar.css">')
   document.body.innerHTML = htmlSrc + document.body.innerHTML
+
+  // User Menu
+
+  document.querySelector('.user.logo').addEventListener('click', () => {
+    document.getElementById('user-menu').classList.toggle('hidden')
+  })
+  // Dirty hack to avoid animation triggering immediately
+  setInterval(() => {
+    document.getElementById('user-menu').classList.add('with-transition')
+  }, 50)
+
+  fetch('/api/auth')
+    .then(r => r.json())
+    .then(r => setUserMenuState(r.status === 'CONNECTED' ? 'connected' : 'login'))
+
+  function setUserMenuState(state) {
+    const userMenuDOM = document.getElementById('user-menu')
+    userMenuDOM.childNodes.forEach(e => {
+      if (e.id === 'user-menu-ghost') return
+      e.classList?.remove('proper-pos')
+      e.classList?.add('hidden-pos')
+      setTimeout(() => {
+        if (userMenuDOM.contains(e)) userMenuDOM.removeChild(e)
+      }, 1000)
+    })
+    const containerDOM = makeContainer()
+    userMenuDOM.prepend(containerDOM)
+    const newHeight = containerDOM.getBoundingClientRect().height + 'px'
+    document.getElementById('user-menu-ghost').style.height = newHeight
+    setTimeout(() => containerDOM.classList.add('proper-pos'), 0)
+
+    function makeContainer() {
+      if (state === 'login') {
+        const formDOM = document.createElement('form')
+        formDOM.innerHTML = `
+          <div class="input">
+            <input type="text" name="nickname">
+          </div>
+          <div class="input">
+            <input type="password" name="password">
+          </div>
+          <div class="input">
+            <input type="submit" value="Se connecter">
+          </div>
+          <button class=signup-instead>Pas de compte?</button>
+        `
+        formDOM.querySelector('.signup-instead').onclick = e => {
+          setUserMenuState('signup')
+          e.preventDefault()
+        }
+        return formDOM
+      }
+      if (state === 'signup') {
+        const formDOM = document.createElement('form')
+        formDOM.innerHTML = `
+          <div class="input">
+            <input type="text" name="nickname">
+          </div>
+          <div class="input">
+            <input type="email" name="email">
+          </div>
+          <div class="input">
+            <input type="password" name="password">
+          </div>
+          <div class="input">
+            <input type="password" name="confirmation">
+          </div>
+          <div class="input">
+            <input type="submit" value="S'inscrire">
+          </div>
+          <button class=login-instead>Déjà un compte?</button>
+        `
+        formDOM.querySelector('.login-instead').onclick = e => {
+          setUserMenuState('login')
+          e.preventDefault()
+        }
+        return formDOM
+      }
+      if (state === 'connected') {
+        const divDOM = document.createElement('div')
+        divDOM.innerHTML = `
+          <button>Mon profil</button>
+        `
+        return divDOM
+      }
+    }
+  }
 }
