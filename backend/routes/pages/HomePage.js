58213801170b5f4c0b51aac8db234360169f5dd1
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-24 13:36:05 +0200

    Home page desc sort

--- backend/routes/pages/HomePage.js
+++ backend/routes/pages/HomePage.js
@@ -3,7 +3,7 @@ import db from '../../database.js'
 export default function homePage({ app }) {
   app.get('/', async (req, res) => {
     const categories = await db('Categories')
-    const recruitments = await db('Recruitments')
+    const recruitments = await db('Recruitments').orderBy('created', 'desc')
     const users = await db('Users') // TODO: filter only needed
     res.render('home', { categories, recruitments, users })
   })
